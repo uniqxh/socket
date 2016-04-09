cca278fb607b325e27325553574bc7fd7d4208ab
uniqxh <525799145@qq.com>
2016-04-09 17:50:32 +0800

    实现web服务器支持静态文件请求

--- epoll.c
+++ epoll.c
@@ -1,7 +1,9 @@
 #include<sys/types.h>
 #include<unistd.h>
+#include<sys/stat.h>
 #include<sys/mman.h>
 #include<sys/socket.h>
+#include<errno.h>
 #include<sys/epoll.h>
 #include<arpa/inet.h>
 #include<netinet/in.h>
@@ -10,23 +12,68 @@
 #include<stdlib.h>
 #include<string.h>
 #define maxevent 10000
+void getFileType(char* fileType, char* fileName){
+	if(strstr(fileName, ".html")) strcpy(fileType, "text/html");
+	else if(strstr(fileName, ".gif")) strcpy(fileType, "image/gif");
+	else if(strstr(fileName, ".jpg")) strcpy(fileType, "image/jpeg");
+	else if(strstr(fileName, ".png")) strcpy(fileType, "image/png");
+	else if(strstr(fileName, ".ico")) strcpy(fileType, "image/x-icon");
+	else strcpy(fileType, "text/plain");
+}
+int writen(int fd,void* buf, size_t n){
+	size_t l = n;
+	size_t w;
+	char* bf = buf;
+	while(l > 0){
+		if((w = write(fd, bf, l)) <= 0){
+			if(errno == EINTR) w = 0;
+			else return -1;
+		}
+		l -= w;
+		bf += w;
+	}
+	return n;
+}
 int setnonblocking(int fd){
 	if(fcntl(fd, F_SETFL, fcntl(fd, F_GETFD, 0) | O_NONBLOCK) == -1){
 		return -1;
 	}
 	return 0;
 }
-int writeWeb(int fd){
-	char buf[2048];
-	char str[] = "<html><title>myTiny web server</title><body><h2>hello, world!welcome to myTiny web server!</h2></body></html>";
-	int len = strlen(str);
+int writeWeb(int fd, char* u){
+	struct stat sb;
+	size_t len = 0;
+	if(stat(u, &sb) >= 0) len = sb.st_size;
+	char fileType[20];
+	getFileType(fileType, u);
+	char buf[2048] = {0};
 	sprintf(buf, "HTTP/1.1 200 OK\r\n");
 	sprintf(buf, "%sServer: myTiny Web Server\r\n", buf);
-	sprintf(buf, "%sContent-length: %d\r\n", buf, len);
-	sprintf(buf, "%sContent-type: text/html; charset=utf-8\r\n\r\n%s", buf, str);
-
-	write(fd, buf, sizeof(buf));
-	puts(buf);
+	sprintf(buf, "%sContent-length: %lu\r\n", buf, len);
+	sprintf(buf, "%sContent-type: %s;charset=utf-8\r\n\r\n",buf, fileType);
+	writen(fd, buf, strlen(buf));
+	printf("[%s]head length: %lu, body length: %lu\n", u, strlen(buf), len);
+	int ffd = open(u, O_RDONLY, 0);
+	if(ffd < 0){
+		printf("file %s not found\n", u);
+		return -1;
+	}
+	char* mp = mmap(0, len, PROT_READ, MAP_PRIVATE, ffd, 0);
+	close(ffd);
+	writen(fd, mp, len);
+	munmap(mp, len);
+	return 0;
+}
+int parseUrl(int fd, char* m, char* u, char* v){
+	if(strcmp(m,"GET") == 0){
+		if(strcmp(u, "/") == 0) sprintf(u, "%sindex.html", u);
+		sscanf(u, "/%s", u);
+		writeWeb(fd, u);
+	}else if(strcmp(m, "POST") == 0){
+		printf("POST response:\n");
+	}else{
+		printf("unknown method\n");
+	}
 	return 0;
 }
 int main(){
@@ -103,7 +150,6 @@ int main(){
 					exit(0);
 				}
 				printf("new accept client %d\n", newfd);
-				writeWeb(newfd);
 				event.data.fd = newfd;
 				event.events = EPOLLIN | EPOLLET;
 				if(epoll_ctl(efd, EPOLL_CTL_ADD, newfd, &event) < 0){
@@ -118,6 +164,11 @@ int main(){
 				close(ev[i].data.fd);
 				epoll_ctl(efd, EPOLL_CTL_DEL, ev[i].data.fd, &event);
 				cfd --;
+			}else{
+				char m[10], u[100], v[20];
+				sscanf(buf,"%s %s %s", m, u, v);
+				printf("%s %s %s\n\r", m, u, v);
+				parseUrl(ev[i].data.fd, m, u, v);
 			}
 		}
 	}
